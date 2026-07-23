c7dfac3dff4fdefa784acd2498a4a2e572be0d69
sumit12312299 <sumitkr4364@gmail.com>
2026-07-23 23:28:33 +0530

    feat(accounts): customize UserAdmin with phone field and search capabilities

--- Parking-Management-System-main/accounts/admin.py
+++ Parking-Management-System-main/accounts/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-admin.site.register(User)
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'phone', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email', 'phone')
+    ordering = ('-date_joined',)
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Extra Details', {'fields': ('phone',)}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Extra Details', {'fields': ('phone',)}),
+    )
